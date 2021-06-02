178df1387a097e29ac790777d6c8c06fd47b8f92
Linda Xiao <lindax28@hotmail.com>
2021-06-01 17:11:43 -0700

    Bootstrap current user

--- frontend/alwayzon.jsx
+++ frontend/alwayzon.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import configureStore from './store/store';
 import Root from './components/root';
-import { login } from './actions/session_actions';
+import { login, logout } from './actions/session_actions';
 
 document.addEventListener('DOMContentLoaded', () => {
   let store;
@@ -20,6 +20,7 @@ document.addEventListener('DOMContentLoaded', () => {
   }
   window.dispatch = store.dispatch;
   window.login = login;
+  window.logout = logout;
   const root = document.getElementById('root');
   ReactDOM.render(<Root store={store}/>, root);
 });
