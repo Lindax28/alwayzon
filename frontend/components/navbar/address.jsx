32245290374a9d4363ae52819c1ea3eb5af811a3
Linda Xiao <lindax28@hotmail.com>
2021-07-15 01:34:45 -0700

    Add responsiveness to upper navbar

--- frontend/components/navbar/address.jsx
+++ frontend/components/navbar/address.jsx
@@ -6,10 +6,10 @@ import { Link } from 'react-router-dom';
 const Address = () => {
   return(
     <div className="address-nav">
-      <Link className="address" to='/login'>
+      <div className="address">
         <p className="nav-small">&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;Hello</p>
         <p className="bold"><LocationIcon/>&nbsp;Select your address</p>
-      </Link>
+      </div>
         <div className="location">
           <p className="location-box">
             <BiCurrentLocation fill="black"/>
